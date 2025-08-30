08b373477f0d8225bce4a8751b78422a15dab40d
Priyanshu ranjan <priyanshuranjan0001@gmail.com>
2025-08-30 08:48:19 +0530

    Add transferMnt function for TON token transfers and update exports

--- src/tools/duck/index.ts
+++ src/tools/duck/index.ts
@@ -1,3 +1,5 @@
 export { getTonBalance } from "./getTonBalance";
 export { getErc20Balance } from "./getErc20Balance";
 export { deployContract } from "./deployContract";
+export { transferTon } from "./tonOperations";
+
